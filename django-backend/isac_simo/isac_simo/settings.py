ccc5deddd2ec0bdf7fb715f87f016d0fdfb933d0
Niush <niushsitaula@gmail.com>
2020-02-19 12:02:39 +0545

    Re-deploy

--- django-backend/isac_simo/isac_simo/settings.py
+++ django-backend/isac_simo/isac_simo/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
